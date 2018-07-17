f32dceaa5e972c77471643dc33b28f601b0fab9d
Joakim Kajsjö <joakim.kajsjo@oneagency.se>
2018-07-17 14:51:07 +0200

    inital commit

---
+++ CinodeApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CinodeApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                   .UseUrls("http://localhost:8080",$"http://{Dns.GetHostAddresses(Dns.GetHostName()).First()}:8080")
+                .Build();
+    }
+}
